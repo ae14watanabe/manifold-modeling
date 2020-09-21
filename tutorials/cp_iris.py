ffce11e2e5688195882f3834f44ab837f171b679
Ryuji Watanabe <watanabe.ryuji717@mail.kyutech.jp>
2020-09-21 17:16:17 +0900

    Scatter latent variables

--- tutorials/cp_iris.py
+++ tutorials/cp_iris.py
@@ -1,8 +1,25 @@
 from gmmzoo.som import SOM
 from sklearn.datasets import load_iris
 import torch
+import matplotlib.pyplot as plt
 def _main():
     iris = load_iris()
     X = iris.data
-    som = SOM(X=torch.tensor(X),n_dim_latent=2,init='pca')
+
+    n_dim_latent=2
+    n_grids=10
+    n_epoch = 10
+    init='pca'
+    shape_latent_space='unit_hypercube'
+    sigma_sheculing = {'max':0.5, 'min': 0.1}
+
+    som = SOM(X=torch.tensor(X),n_dim_latent=n_dim_latent,init=init,
+              shape_latent_space=shape_latent_space,n_grids=n_grids,n_epoch=n_epoch,
+              schedule_sigma=sigma_sheculing)
+
+    som.fit()
+    fig = plt.figure()
+    ax = fig.add_subplot(111,aspect='equal')
+    ax.scatter(som.ls.data[:, 0], som.ls.data[:, 1])
+    plt.show()
 if __name__ == '__main__':
